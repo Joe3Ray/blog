8b8ca789478cbbf3e6c1ff86d93e386b80de6b44
JoeRay61 <791401115@qq.com>
2018-06-05 23:03:14 +0800

    feat: 新增分类列表页面

--- app/components/categories/index.js
+++ app/components/categories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import styles from './index.less'
 
@@ -7,7 +7,20 @@ export default class Categories extends React.PureComponent {
   render () {
     return (
       <div className={styles['categories-wrapper']}>
-        123
+        <header>分类</header>
+        <p className={styles['categories-summary']}>
+          目前共计 1 个分类
+        </p>
+        <ul>
+          <li>
+            <Link className={styles['categories-link']} to='/categories/技术'>技术</Link>
+            <span>(17)</span>
+          </li>
+          <li>
+            <Link className={styles['categories-link']} to='/categories/技术'>技术</Link>
+            <span>(17)</span>
+          </li>
+        </ul>
       </div>
     )
   }
